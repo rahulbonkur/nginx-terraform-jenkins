49fe699b05be247fc1e1ec581cd69cb31aa13c88
rahulbonkur <rahulbonkur@gmail.com>
2026-01-14 01:14:50 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  default = "ap-south-1a"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+}
